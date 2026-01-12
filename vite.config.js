f1d88d08723e62110a87f52ebf740a77695ebed8
its-priya-here <priyasingh.sp98@gmail.com>
2026-01-12 14:38:24 +0530

    Fix blank page - root deployment

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/chat-app/',
-  build: {
-    outDir: 'docs',              
-  },
+  base: '/',
   plugins: [react()],
 })
